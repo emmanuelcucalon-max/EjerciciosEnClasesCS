66359bcd2e1b4749540009206aff6cf678303fa0
emmanuelcucalon-max <ercucalon@uamv.edu.ni>
2026-05-10 20:24:58 -0600

    feat:ejerciciosasignadosenclases

---
+++ magnituddeunvector/Program.cs
@@ -0,0 +1,31 @@
+﻿double[] Vector1 = new double [5];
+double SumaCuadrados = 0;
+double Magnitud = 0;
+Console.WriteLine("Ingrese los elementos del vector:");
+for (int i = 0; i < Vector1.Length; i++)
+{
+    try
+    {
+        Console.Write($"Elemento {i + 1}: ");
+        Vector1[i] = int.Parse(Console.ReadLine()!);
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Entrada no válida. Por favor, ingrese un número entero.");
+        i--; // Decrementar el índice para volver a solicitar el mismo elemento
+    }
+
+}
+for (int i=0; i<Vector1.Length; i++)
+{
+    Vector1[i]= Math.Pow(Vector1[i], 2);
+}
+for (int i=0; i<Vector1.Length; i++)
+{
+    SumaCuadrados += Vector1[i];
+}
+for (int i=0; i<Vector1.Length; i++)
+{
+    Magnitud = Math.Sqrt(SumaCuadrados);
+}
+Console.WriteLine($"La magnitud del vector es: {Magnitud}");
---
+++ productoescalar/Program.cs
@@ -0,0 +1,37 @@
+﻿int[] Vector1 = new int[5];
+int[] Vector2 = new int[5];
+int ProdutoEscalar = 0;
+
+Console.WriteLine("Ingrese los valores del primer vector:");
+for (int i=0;i<Vector1.Length;i++)
+{
+    try
+    {
+        Console.WriteLine($"Elemento {i+1}");
+        Vector1[i]=int.Parse(Console.ReadLine()!);
+    }
+    catch(FormatException)
+    {
+        Console.WriteLine("Formato incorrecto, Escriba en un formato adecuado");
+        i--;
+    }
+}
+Console.WriteLine("Ingrese los valores del Segundo vector:");
+for (int i=0;i<Vector2.Length;i++)
+{
+    try
+    {
+        Console.WriteLine($"Elemento {i+1}");
+        Vector2[i]=int.Parse(Console.ReadLine()!);
+    }
+    catch(FormatException)
+    {
+        Console.WriteLine("Formato incorrecto, Escriba en un formato adecuado");
+        i--;
+    }
+}
+for(int i=0;i<Vector1.Length;i++)
+{
+    ProdutoEscalar+=Vector1[i]*Vector2[i];
+}
+Console.WriteLine($"La magnitud escalar de los 2 vectores es de: {ProdutoEscalar}");
